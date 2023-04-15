e2e68922dec29a92779cff97cc87f669b25c6188
keishibuki <kei.play52@gmail.com>
2023-04-15 10:43:04 +0900

    ブロックを複数想定に

--- wp-splide.php
+++ wp-splide.php
@@ -12,6 +12,7 @@
  *
  * @package           create-block
  */
+define( "BUILD_PATH", "/build" );
 
 /**
  * Registers the block using the metadata loaded from the `block.json` file.
@@ -21,6 +22,30 @@
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
 function create_block_wp_splide_block_init() {
-	register_block_type( __DIR__ . '/build' );
+
+	register_block_type( __DIR__ . BUILD_PATH . "/blocks/simple"  );
 }
 add_action( 'init', 'create_block_wp_splide_block_init' );
+
+add_action( 'admin_enqueue_scripts', function () {
+	// CSS
+	wp_enqueue_style(
+		'custom-admin-css',
+		__DIR__ . '/build/admin.css'
+	);
+} );
+
+//ブロックカテゴリーの追加
+function add_block_categories( $block_categories, $block_editor_context ) {
+    array_push(
+		$block_categories,
+		array(
+			'slug'  => 'wp-splide',
+			'title' => 'WP Splide',
+			'icon'  => 'null',
+		)
+	);
+
+	return $block_categories;
+}
+add_filter( 'block_categories_all', 'add_block_categories', 10, 2 );
